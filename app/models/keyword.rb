0df7f50d8538898a0d904494e54e4a4f8c46a926
Shrihari <smurlidharan3@gatech.edu>
2013-04-25 23:37:26 -0400

    Shrihari: The web app now talks to Facebook api

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,7 +1,24 @@
+require 'net/http'
+
 class Keyword < ActiveRecord::Base
   def query_solr(keyword)
-    solr = RSolr.connect :url => 'http://192.168.1.3:8983/solr'
-    response = solr.get 'select', :params => {:q => 'tweet:' + keyword}
-    response.to_s
+#    solr = RSolr.connect :url => 'http://192.168.1.3:8983/solr'
+#    response = solr.get 'select', :params => {:q => 'tweet:' + keyword}
+#    response.to_s
+    facebook_data(keyword)
+  end
+
+  private
+  def facebook_data(keyword)
+    uri = URI.parse('http://localhost/fb/example1.php')
+    params = {'movie_name' => keyword}
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Get.new(uri.path)
+    request.set_form_data( params )
+
+    request = Net::HTTP::Get.new( uri.path+ '?' + request.body )
+
+    response = http.request(request)
+    response.body
   end
 end
